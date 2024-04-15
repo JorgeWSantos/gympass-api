aeb06e964d14c52d611011ad6524328170654b54
JorgeWS <jorgews.dev@gmail.com>
2024-04-15 07:48:26 -0300

    test: 🧪 Add first suit of tests

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,7 +1,58 @@
-import { test, expect } from "vitest";
+import { expect, describe, it } from "vitest";
+import { RegisterService } from "./register";
+import { compare } from "bcryptjs";
+import { MockUsersRepository } from "@/repositories/mocks/MockUsersRepository";
+import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
 
 import("vite");
 
-test("check if it works", () => {
-  expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+  it("should be able to create user", async () => {
+    const usersRepository = new MockUsersRepository();
+    const registerUseCase = new RegisterService(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "johndoe",
+      email: "jhondoe@gmail.com",
+      password: "123",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new MockUsersRepository();
+    const registerUseCase = new RegisterService(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "johndoe",
+      email: "jhondoe@gmail.com",
+      password: "123",
+    });
+
+    const isPasswordCorrectlyHashed = await compare("123", user.password_hash);
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it("shouldn't be possible register two users with same email", async () => {
+    const usersRepository = new MockUsersRepository();
+    const registerUseCase = new RegisterService(usersRepository);
+
+    const email = "jhondoe@gamil.com";
+
+    await registerUseCase.execute({
+      name: "johndoe",
+      email,
+      password: "123",
+    });
+
+    expect(() =>
+      registerUseCase.execute({
+        name: "johndoe",
+        email,
+        password: "123",
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
 });
