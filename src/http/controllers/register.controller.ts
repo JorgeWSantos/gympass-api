ec5d205a045ab67c52878159481510b3d72e4c0c
JorgeWS <jorgews.dev@gmail.com>
2024-04-20 04:38:47 -0300

    feat: 🎸 Add Factory to register controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { RegisterService } from "@/services/register-service";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists-error";
+import { makeRegisterService } from "@/services/factories/make-register-service";
 
 export const register = async (
   request: FastifyRequest,
@@ -17,10 +16,9 @@ export const register = async (
 
     const { email, name, password } = userBodySchema.parse(request.body);
 
-    const usersRepository = new PrismaUsersRepository();
-    const registerService = new RegisterService(usersRepository);
+    const service = makeRegisterService();
 
-    await registerService.execute({ email, name, password });
+    await service.execute({ email, name, password });
 
     return reply.status(201).send();
   } catch (error) {
