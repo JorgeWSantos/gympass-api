e1229f73b7d9aade2633d6a49eb9e72bd647864b
JorgeWS <jorgews.dev@gmail.com>
2024-04-20 04:38:30 -0300

    feat: 🎸 Create authenticate controller

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,29 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error";
+import { makeAuthenticateService } from "@/services/factories/make-authenticate-service";
+
+export const authenticate = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  try {
+    const authenticateBodySchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    });
+
+    const { email, password } = authenticateBodySchema.parse(request.body);
+
+    const service = makeAuthenticateService();
+
+    await service.execute({ email, password });
+
+    return reply.status(200).send();
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError)
+      return reply.status(400).send({ message: error.message });
+
+    return reply.status(500).send({ message2: error.message }); // TODO:
+  }
+};
